dab8fd7013519b9e089f64c1d16c7cfe320ee1c9
yqu1 <quyaoxian@gmail.com>
2017-01-14 19:45:44 -0500

    jaja

--- config.js
+++ config.js
@@ -4,6 +4,6 @@ module.exports = {
     'facebook': {
     	clientID: '1122103044479613',
     	clientSecret: 'a35a2da487b2f03234eee837dc1b4fcc',
-    	callbackURL: 'https://localhost:3443/users/facebook/callback'
+    	callbackURL: 'https://peaceful-fortress-87755.herokuapp.com/users/facebook/callback'
     }
 }
