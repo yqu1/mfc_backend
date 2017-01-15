f615822201b0f606b8600b1f5ddc1e674f68464c
yqu1 <quyaoxian@gmail.com>
2017-01-14 19:51:05 -0500

    fb

--- config.js
+++ config.js
@@ -2,8 +2,8 @@ module.exports = {
     'secretKey': '12345-67890-09876-54321',
     'mongoUrl' : 'mongodb://yqu1:qazwsx01@ds157487.mlab.com:57487/mfc',
     'facebook': {
-    	clientID: '1122103044479613',
-    	clientSecret: 'a35a2da487b2f03234eee837dc1b4fcc',
+    	clientID: '764077030407819',
+    	clientSecret: '50d221613a36141aa147c57e3ddffe6d',
     	callbackURL: 'https://peaceful-fortress-87755.herokuapp.com/users/facebook/callback'
     }
 }
